f3d40cdd12cccbf8b1a0bc8fd54b8a2d555e0cee
Manuel Muñoz <heidiaricel@hotmail.com>
2019-04-21 10:55:53 +0200

    Añadida versión preliminar del menú lateral
    Se ha creado un componente llamado "DrawerNavigator" que se utilizará para el menú lateral de la aplicación. Este componente se modificará repetidamente hasta su configuración final

---
+++ src/screens/DrawerNavigator/DrawerNavigator.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  Image,
+  ImageBackground,
+  AsyncStorage
+} from "react-native";
+import { connect } from "react-redux";
+
+// REACT NAVIGATION
+import { createDrawerNavigator } from "react-navigation";
+
+// IMPORTAR LOS COMPONENTES DE NATIVE BASE
+import {
+  Container,
+  Content,
+  Text,
+  List,
+  ListItem,
+  Icon,
+  Left,
+  Body,
+  Right,
+  Button,
+  Badge,
+  Separator,
+  View
+} from "native-base";
+
+import DrawerNavigatorStyles from "./DrawerNavigatorStyles";
+
+// IMPORTAR LAS SCREEN DEL DRAWERNAVIGATOR
+import TabFirst from "../TabFirst/TabFirst";
+import TabSecond from "../TabSecond/TabSecond";
+
+class DrawerNavigator extends Component {
+  state = {
+    appJson: ""
+  };
+
+  render() {
+    return (
+      <Container>
+        <Content>
+        <ImageBackground
+              source={require("../../../assets/img/fondologomenu.jpg")}
+            style={
+              DrawerNavigatorStyles.background
+            }
+          >
+            {/* <Image
+              square
+              style={
+                DrawerNavigatorStyles.background_image
+              }
+              source={require("../../../assets/img/yay.jpg")}
+            /> */}
+            <Image
+            square
+            style={{ width: 80, height: 80 }}
+            source={require("../../../assets/img/yay.jpg")}
+          />
+            <Text
+              style={
+                DrawerNavigatorStyles.background_text
+              }
+            >
+              {/* {this.state.user ? this.state.user.attributes.email : null} */}
+              micorreo@gmail.com
+            </Text>
+          </ImageBackground>
+          <ListItem
+            icon
+            button
+            onPress={() => this.props.navigation.navigate("TabFirst")}
+          >
+            <Left>
+              <Button style={{ backgroundColor: "#8F8E93" }}>
+                <Icon name="notifications" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>TabFirst</Text>
+            </Body>
+            <Right>
+              <Text>1</Text>
+            </Right>
+          </ListItem>
+          <ListItem
+            icon
+            button
+            onPress={() => this.props.navigation.navigate("TabSecond")}
+          >
+            <Left>
+              <Button style={{ backgroundColor: "#4CDA64" }}>
+                <Icon name="home" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>TabSecond</Text>
+            </Body>
+            <Right>{/* <Icon active name="arrow-forward" /> */}</Right>
+          </ListItem>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    appJson: state.mainReducer.appJson
+  };
+};
+
+export default connect(mapStateToProps)(DrawerNavigator);
+
+export const AppDrawerNavigator = createDrawerNavigator(
+  {
+    TabFirst: TabFirst,
+    TabSecond: TabSecond
+  },
+  {
+    contentComponent: DrawerNavigator
+  }
+);
---
+++ src/screens/DrawerNavigator/DrawerNavigatorStyles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  // Imagen de background para el menu
+  background: {
+    height: 120,
+    alignSelf: "stretch",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  },
+  // Estilo para la imagen
+  background_image: {
+    width: "100%",
+    height: 50
+  },
+  // Estilo para la el texto
+  background_text: {
+    color: "#FFF",
+    marginTop: 5,
+    textAlign: "center",
+  },
+});

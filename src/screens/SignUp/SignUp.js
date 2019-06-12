a3dca9a0b79e7fb0546a8c2127386ea244481075
Manuel Muñoz <heidiaricel@hotmail.com>
2019-06-12 19:14:54 +0200

    Modificada pantalla "SignUp"
    Se ha modificado la pantalla de SignUp, se ha añadido la funcionalidad necesaria para comunicarse con la Api de YAY

--- src/screens/SignUp/SignUp.js
+++ src/screens/SignUp/SignUp.js
@@ -16,6 +16,8 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 
 import SignUpStyles from "./SignUpStyles";
 import * as Validar from "../../functions/Validate_helper";
+import * as AppConsts from "../../../config/app_consts";
+import * as YAY_Api from "../../functions/YAY_Api_helper";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
@@ -27,6 +29,7 @@ class SignUp extends Component {
   state = {
     appJson: "",
     loading_process_bar: false, // Muestra el simbolo de cargando
+    loading_sign_up: false,
     nombre: "",
     nombre_error: false,
     apellidos: "",
@@ -61,12 +64,11 @@ class SignUp extends Component {
     }
 
     // VALIDACIÓN DEL NUMERO
-    if (this.state.telefono.length != 9){
-      error= true;
-      this.setState({telefono_error:true})
-    }
-    else{
-      this.setState({telefono_error:false})
+    if (this.state.telefono.length != 9) {
+      error = true;
+      this.setState({ telefono_error: true });
+    } else {
+      this.setState({ telefono_error: false });
     }
 
     if (!Validar.validateEmail(this.state.email)) {
@@ -92,23 +94,17 @@ class SignUp extends Component {
       Alert.alert(
         "Account activation",
         this.state.email +
-          "\n" +
           "Make sure the email is correct. It is necessary to validate the account.",
         [
-          {
-            text: "Cancel"
-          },
+          { text: "Cancel" },
           {
             text: "OK",
             onPress: () => {
-              console.log("Todo va bien");
               this.signUp();
             }
           }
         ],
-        {
-          cancelable: true
-        }
+        { cancelable: true }
       );
     } else {
       console.log("Se han encontrado errores");
@@ -116,8 +112,33 @@ class SignUp extends Component {
     }
   };
 
-  signUp = () => {
-    this.props.navigation.navigate("Home");
+  signUp = async () => {
+    signUpJSON = {
+      email: this.state.email,
+      password: this.state.password,
+      first_name: this.state.nombre,
+      last_name: this.state.apellidos,
+      phone_number: this.state.telefono
+    };
+
+    this.setState({ loading_sign_up: true });
+
+    let response = await YAY_Api.fetchInternetDataAsync(
+      AppConsts.URL_REGISTER,
+      await YAY_Api.getRequestPostAsync(signUpJSON)
+    );
+    this.setState({ loading_sign_up: false });
+
+    if (response.status === "success") {
+      console.log("Registrado correctamente");
+    } else if (
+      response.status === "error" &&
+      response.error === "USER_ALREADY_REGISTERED"
+    ) {
+      console.log("Usuario ya registrado");
+    } else {
+      console.log("Error desconocido");
+    }
   };
 
   render() {
@@ -158,11 +179,9 @@ class SignUp extends Component {
             onSubmitEditing={() => {
               this.lastNameInput.focus();
             }}
-            onChangeText={value => this.setState({nombre: value})}
+            onChangeText={value => this.setState({ nombre: value })}
             errorMessage={
-              this.state.nombre_error
-                ? "Please enter a First Name"
-                : null
+              this.state.nombre_error ? "Please enter a First Name" : null
             }
             containerStyle={SignUpStyles.input_container}
             inputContainerStyle={SignUpStyles.input_container_style}
@@ -189,11 +208,9 @@ class SignUp extends Component {
             onSubmitEditing={() => {
               this.phoneInput.focus();
             }}
-            onChangeText={value => this.setState({apellidos: value})}
+            onChangeText={value => this.setState({ apellidos: value })}
             errorMessage={
-              this.state.apellidos_error
-                ? "Please enter a Last Name"
-                : null
+              this.state.apellidos_error ? "Please enter a Last Name" : null
             }
             containerStyle={SignUpStyles.input_container}
             inputContainerStyle={SignUpStyles.input_container_style}
@@ -202,7 +219,7 @@ class SignUp extends Component {
             keyboardAppearance="light"
             blurOnSubmit={false}
           />
-                    <Input
+          <Input
             leftIcon={
               <Icon
                 name="screen-smartphone"
@@ -220,11 +237,9 @@ class SignUp extends Component {
             onSubmitEditing={() => {
               this.email2Input.focus();
             }}
-            onChangeText={value => this.setState({telefono: value})}
+            onChangeText={value => this.setState({ telefono: value })}
             errorMessage={
-              this.state.telefono_error
-                ? "Please enter a Phone Number"
-                : null
+              this.state.telefono_error ? "Please enter a Phone Number" : null
             }
             containerStyle={SignUpStyles.input_container}
             inputContainerStyle={SignUpStyles.input_container_style}
@@ -316,7 +331,7 @@ class SignUp extends Component {
             returnKeyType="done"
             ref={input => (this.confirmPassword2Input = input)}
             onSubmitEditing={this.signUpDataConfirmation}
-            onChangeText={value => this.setState({password_copy: value})}
+            onChangeText={value => this.setState({ password_copy: value })}
             errorMessage={
               this.state.password_copy_error
                 ? "Your passwords doesn't match"
@@ -330,7 +345,7 @@ class SignUp extends Component {
             blurOnSubmit={true}
           />
           <Button
-            loading={false}
+            loading={this.state.loading_sign_up}
             title="SIGNUP"
             containerStyle={SignUpStyles.button_signup_container_style}
             buttonStyle={SignUpStyles.button_signup_style}
@@ -344,7 +359,7 @@ class SignUp extends Component {
             containerStyle={SignUpStyles.button_container_style}
             buttonStyle={SignUpStyles.button_style}
             titleStyle={SignUpStyles.button_title_style}
-            onPress={()=>this.props.navigation.navigate("SignIn")}
+            onPress={() => this.props.navigation.navigate("SignIn")}
             disabled={false}
           />
         </View>

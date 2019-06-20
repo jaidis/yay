83331470a50df148fd0522fa0d9ddb53d4d0d239
Manuel Muñoz <heidiaricel@hotmail.com>
2019-06-20 20:42:50 +0200

    Modificada pantalla Bookings
    Se ha modificado la pantalla Bookings, se ha añadido la comprobación de NetInfo para refrescar los datos del listado de las  reservas
    
    Se han añadido nuevas traducciones a los ficheros
    
    Se han movido los estilos al fichero correspondiente

--- src/screens/Bookings/Bookings.js
+++ src/screens/Bookings/Bookings.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 import { View } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
 import { connect } from "react-redux";
 import {
-  loadingTrue,
-  loadingFalse,
   addBookings,
   addReserva,
   deleteReserva
 } from "../../store/actions/index";
 
 import { Icon } from "react-native-elements";
-
 import {
   Container,
-  Header,
   Content,
   List,
   ListItem,
@@ -24,17 +21,21 @@ import {
   Text
 } from "native-base";
 
+// FUNCTIONS OR HELPERS
 import * as NaviteBaseMenu from "../../functions/NativeBaseMenu_helper";
 import * as AppConsts from "../../../config/app_consts";
 import * as YAY_Api from "../../functions/YAY_Api_helper";
 
-import BookingsStyles from "./BookingsStyles";
+// LANGUAGES LIBRARY
+import { setI18nConfig } from "../../../languages/i18n";
+var i18n = setI18nConfig();
 
-import reserva from "../../../reserva.json";
+// STYLES
+import BookingsStyles from "./BookingsStyles";
 
 class Bookings extends Component {
   static navigationOptions = {
-    title: "Bookings"
+    title: i18n.t("bookings_title")
   };
 
   state = {
@@ -53,20 +54,30 @@ class Bookings extends Component {
       this.setState({ reservas_disponibles: false });
     }
 
-    try {
-      bookingJSON = { token: this.props.appJson.userdata.token };
-      let response = await YAY_Api.fetchInternetDataAsync(
-        AppConsts.URL_BOOKINGS_SEARCH,
-        await YAY_Api.getRequestPostAsync(bookingJSON)
-      );
-
-      if (response.data.length > 0) {
-        this.props.c_addBookings(response);
-        this.setState({ reservas_disponibles: true, bookings: response });
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    NetInfo.isConnected
+      .fetch()
+      .then(async isConnected => {
+        if (isConnected) {
+          try {
+            bookingJSON = { token: this.props.appJson.userdata.token };
+            let response = await YAY_Api.fetchInternetDataAsync(
+              AppConsts.URL_BOOKINGS_SEARCH,
+              await YAY_Api.getRequestPostAsync(bookingJSON)
+            );
+            if (response.status === "success") {
+              this.props.c_addBookings(response);
+              this.setState({ reservas_disponibles: true, bookings: response });
+            } else {
+              console.log(response);
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   /**
@@ -78,11 +89,11 @@ class Bookings extends Component {
 
     this.subs = [
       this.props.navigation.addListener("willFocus", async () => {
-        console.log("BOOKINGS, inicio");
+        // console.log("BOOKINGS, inicio");
         this.loadContent();
       }),
       this.props.navigation.addListener("willBlur", async () => {
-        console.log("BOOKINGS, me voy");
+        // console.log("BOOKINGS, me voy");
       })
     ];
   }
@@ -93,20 +104,32 @@ class Bookings extends Component {
    */
   componentWillUnmount() {
     this.subs.forEach(sub => sub.remove());
-    console.log("BOOKINGS, Desmontado");
+    // console.log("BOOKINGS, Desmontado");
     this.props.c_deleteReserva();
   }
-
+  /**
+   * @description Cambia la fecha a formato español
+   * @param {*} date
+   */
   formatDate(date) {
     let splitedDate = date.split("-");
     return splitedDate[2] + "-" + splitedDate[1] + "-" + splitedDate[0];
   }
 
+  /**
+   * @description Guarda la reserva en Redux y muestra la pantalla de Reserva
+   * @param {*} reserva
+   */
   detailBooking(reserva) {
     this.props.c_addReserva(reserva);
     this.props.navigation.navigate("Reserve");
   }
 
+  /**
+   * @description Devuelve elementos de una lista con la información de la reserva maquetada
+   * @param {*} reserva
+   * @param {*} index
+   */
   renderReservas(reserva, index) {
     return (
       <ListItem
@@ -122,12 +145,23 @@ class Bookings extends Component {
         </Left>
         <Body>
           <Text>{reserva.restaurant_name}</Text>
-          <Text note>Diners: {reserva.diners}</Text>
-          <Text note>Date: {this.formatDate(reserva.reserve_date)}</Text>
-          <Text note>Hour: {reserva.reserve_time}</Text>
-          <Text note>Status: {reserva.status}</Text>
+          <Text note>
+            {i18n.t("bookings_list_item_diners")} {reserva.diners}
+          </Text>
+          <Text note>
+            {i18n.t("bookings_list_item_date")}
+            {this.formatDate(reserva.reserve_date)}
+          </Text>
+          <Text note>
+            {i18n.t("bookings_list_item_hour")}
+            {reserva.reserve_time}
+          </Text>
+          <Text note>
+            {i18n.t("bookings_list_item_status")}
+            {reserva.status}
+          </Text>
         </Body>
-        <Right style={{ justifyContent: "center", alignItems: "center" }}>
+        <Right style={BookingsStyles.bookings_list_right}>
           <Icon name="chevron-right" size={30} />
         </Right>
       </ListItem>
@@ -139,9 +173,9 @@ class Bookings extends Component {
    */
   renderEmpty = () => {
     return (
-      <View style={BookingsStyles.favorites_empty}>
-        <Text style={BookingsStyles.favorites_empty_text}>
-          No existen reservas
+      <View style={BookingsStyles.bookings_empty}>
+        <Text style={BookingsStyles.bookings_empty_text}>
+        {i18n.t("bookings_list_item_date")}
         </Text>
       </View>
     );
@@ -150,11 +184,11 @@ class Bookings extends Component {
   render() {
     return (
       <View style={BookingsStyles.container}>
-        {NaviteBaseMenu.menuGoHome(this, "Bookings")}
+        {NaviteBaseMenu.menuGoHome(this, i18n.t("bookings_title"))}
         {this.state.reservas_disponibles ? (
-          <Container style={{ backgroundColor: "#2E3248" }}>
+          <Container style={BookingsStyles.bookings_container}>
             <Content>
-              <List style={{ backgroundColor: "white" }}>
+              <List style={BookingsStyles.bookings_list}>
                 {this.state.bookings.data.map((reserva, index) => {
                   return this.renderReservas(reserva, index);
                 })}
--- src/screens/Bookings/BookingsStyles.js
+++ src/screens/Bookings/BookingsStyles.js
@@ -1,19 +1,35 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  //
   container: {
     flex: 1,
     backgroundColor: "#2E3248"
   },
-  favorites_empty: {
+  //
+  bookings_container: {
+    backgroundColor: "#2E3248"
+  },
+  //
+  bookings_list: {
+    backgroundColor: "white"
+  },
+  //
+  bookings_list_right: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  //
+  bookings_empty: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
   },
-  favorites_empty_text: {
+  //
+  bookings_empty_text: {
     fontSize: 20,
     color: "#FFF",
     textAlign: "center",
     padding: 10
-  },
+  }
 });

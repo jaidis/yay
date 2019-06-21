fc4095af5497f04bae9234d1b5c312ee6d41ee52
Manuel Muñoz <heidiaricel@hotmail.com>
2019-06-21 21:19:44 +0200

    Modificada pantalla Reserve
    Se ha modificado la pantalla Reserve, se ha añadido nuevas traducciones a los ficheros y se han movido los estilos al fichero correspondiente

--- src/screens/Reserve/Reserve.js
+++ src/screens/Reserve/Reserve.js
@@ -1,40 +1,36 @@
 import React, { Component } from "react";
-import { View, ScrollView, Image } from "react-native";
+import { View, ScrollView, Image, Alert } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
 import { connect } from "react-redux";
-import {
-  loadingTrue,
-  loadingFalse,
-  deleteReserva,
-  addBookings
-} from "../../store/actions/index";
+import { deleteReserva, addBookings } from "../../store/actions/index";
 
 import {
-  Container,
-  Header,
   Content,
   Card,
   CardItem,
   DatePicker as DatePickerNativeBase,
   Picker,
-  Text,
-  Right,
-  Body,
-  H2,
   H3
 } from "native-base";
 import { Icon, Input, Button } from "react-native-elements";
 
 import DatePicker from "react-native-datepicker";
 
+// FUNCTIONS OR HELPERS
 import * as NaviteBaseMenu from "../../functions/NativeBaseMenu_helper";
 import * as AppConsts from "../../../config/app_consts";
 import * as YAY_Api from "../../functions/YAY_Api_helper";
 
+// LANGUAGES LIBRARY
+import { setI18nConfig } from "../../../languages/i18n";
+var i18n = setI18nConfig();
+
+// STYLES
 import ReserveStyles from "./ReserveStyles";
 
 class Reserve extends Component {
   static navigationOptions = {
-    title: "Reserve"
+    title: i18n.t("reserve_view_title")
   };
 
   state = {
@@ -49,6 +45,9 @@ class Reserve extends Component {
     button_reserve: true
   };
 
+  /**
+   * @description
+   */
   checkData() {
     try {
       this.setState({
@@ -67,7 +66,7 @@ class Reserve extends Component {
       if (this.props.appJson.userdata.credit_card.length > 0) {
         this.setState({ button_reserve: false });
       }
-      console.log("Datos comprobados");
+      // console.log("Datos comprobados");
     } catch {
       error => {
         console.log("Error when try to retrieve user from Redux: \n" + error);
@@ -84,12 +83,16 @@ class Reserve extends Component {
 
     this.subs = [
       this.props.navigation.addListener("willFocus", async () => {
-        console.log("RESERVE, inicio");
+        // console.log("RESERVE, inicio");
         this.checkData();
       }),
       this.props.navigation.addListener("willBlur", async () => {
-        console.log("RESERVE, me voy");
-        this.props.c_deleteReserva();
+        // console.log("RESERVE, me voy");
+        try {
+          this.props.c_deleteReserva();
+        } catch (error) {
+          console.log(error);
+        }
       })
     ];
   }
@@ -100,10 +103,17 @@ class Reserve extends Component {
    */
   componentWillUnmount() {
     this.subs.forEach(sub => sub.remove());
-    console.log("RESERVE, Desmontado");
-    this.props.c_deleteReserva();
+    // console.log("RESERVE, Desmontado");
+    try {
+      this.props.c_deleteReserva();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  /**
+   * @description
+   */
   encryptedCard = creditCard => {
     let temp = creditCard.split(" ");
     return temp[0] + " xxxx xxxx " + temp[3];
@@ -155,37 +165,65 @@ class Reserve extends Component {
     this.setState({ fechaReserva: myDate });
   }
 
+  /**
+   * @description
+   */
   setCreditCard = value => {
     this.setState({ id_credit_card: value });
   };
 
+  /**
+   * @description
+   */
   registerBooking = async () => {
-    this.setState({ loading_reserve: true });
-    bookingJSON = {
-      token: this.props.appJson.userdata.token,
-      reserve_date: this.state.fechaReserva,
-      reserve_time: this.state.horaReserva,
-      diners: this.state.comensales,
-      id_restaurant: this.props.restaurantJson.data.id,
-      id_credit_card: this.state.id_credit_card
-    };
-    console.log("id_credit_card", this.state.id_credit_card);
-    response = await YAY_Api.fetchInternetDataAsync(
-      AppConsts.URL_BOOKINGS_REGISTER,
-      await YAY_Api.getRequestPostAsync(bookingJSON)
-    );
-    console.log(response);
-    if (response.status === "success") {
-      this.props.c_addBookings(response);
-      setTimeout(() => {
-        this.setState({ loading_reserve: false });
-        this.props.navigation.navigate("ReserveConfirmed");
-      }, 300);
-    } else {
-      console.log("Algo va mal", response);
-      this.setState({ loading_reserve: false });
-    }
-    
+    NetInfo.isConnected
+      .fetch()
+      .then(async isConnected => {
+        if (isConnected) {
+          try {
+            this.setState({ loading_reserve: true });
+            bookingJSON = {
+              token: this.props.appJson.userdata.token,
+              reserve_date: this.state.fechaReserva,
+              reserve_time: this.state.horaReserva,
+              diners: this.state.comensales,
+              id_restaurant: this.props.restaurantJson.data.id,
+              id_credit_card: this.state.id_credit_card
+            };
+
+            // console.log("id_credit_card", this.state.id_credit_card);
+            let response = await YAY_Api.fetchInternetDataAsync(
+              AppConsts.URL_BOOKINGS_REGISTER,
+              await YAY_Api.getRequestPostAsync(bookingJSON)
+            );
+            // console.log(response);
+
+            if (response.status === "success") {
+              this.props.c_addBookings(response);
+              setTimeout(() => {
+                this.setState({ loading_reserve: false });
+                this.props.navigation.navigate("ReserveConfirmed");
+              }, 300);
+            } else {
+              console.log(response);
+              this.setState({ loading_reserve: false });
+            }
+          } catch (error) {
+            console.log(error);
+            this.setState({ loading_reserve: false });
+          }
+        } else {
+          Alert.alert(
+            i18n.t("internet_error_word"),
+            i18n.t("internet_error_message"),
+            [{ text: "OK" }],
+            { cancelable: false }
+          );
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   render() {
@@ -195,33 +233,36 @@ class Reserve extends Component {
           this,
           this.props.reservaJSON
             ? this.props.reservaJSON.restaurant_name
-            : this.props.restaurantJson.data.name
+            : this.props.restaurantJson
+            ? this.props.restaurantJson.data.name
+            : i18n.t("reserve_view_title")
         )}
         <ScrollView>
-          <View style={{ flex: 1, margin: 20 }}>
+          <View style={ReserveStyles.reserve_scroll_view}>
             <Content>
-              <Card style={{ paddingBottom: 20 }}>
+              <Card style={ReserveStyles.reserve_card_style}>
                 <CardItem cardBody>
                   <Image
                     source={{
                       uri: this.props.reservaJSON
                         ? this.props.reservaJSON.restaurant_logo
-                        : this.props.restaurantJson.data.logo
+                        : this.props.restaurantJson
+                        ? this.props.restaurantJson.data.logo
+                        : null
                     }}
-                    style={{ flex: 1, height: 200, width: null }}
+                    style={ReserveStyles.reserve_card_image}
                   />
                 </CardItem>
                 <CardItem
                   cardBody
-                  style={{
-                    marginTop: 30,
-                    alignContent: "center",
-                    justifyContent: "center"
-                  }}
+                  style={ReserveStyles.reserve_card_item_title_style}
                 >
-                  <H3>Datos de la reserva</H3>
+                  <H3>{i18n.t("reserve_booking_title").toUpperCase()}</H3>
                 </CardItem>
-                <CardItem cardBody style={{ marginTop: 20 }}>
+                <CardItem
+                  cardBody
+                  style={ReserveStyles.reserve_card_item_style}
+                >
                   <Content>
                     <View style={ReserveStyles.view_form}>
                       {/* Fecha de Nacimiento */}
@@ -244,7 +285,7 @@ class Reserve extends Component {
                             <DatePickerNativeBase
                               minimumDate={new Date()}
                               defaultDate={this.state.fechaReserva}
-                              locale={"en"}
+                              locale={"es"}
                               timeZoneOffsetInMinutes={undefined}
                               modalTransparent={false}
                               animationType={"fade"}
@@ -276,23 +317,18 @@ class Reserve extends Component {
                             </View>
 
                             <DatePicker
-                              style={{ width: 200 }}
+                              style={ReserveStyles.reserve_date_picker_style}
                               date={this.state.horaReserva}
                               mode="time"
                               format="HH:mm"
-                              confirmBtnText="Confirm"
-                              cancelBtnText="Cancel"
+                              confirmBtnText={i18n.t("reserve_date_picker_confirm")}
+                              cancelBtnText={i18n.t("reserve_date_picker_cancel")}
                               minuteInterval={10}
                               onDateChange={time => {
                                 this.setState({ horaReserva: time });
                               }}
                               customStyles={{
-                                dateInput: {
-                                  borderColor: "#FFF",
-                                  alignItems: "flex-start",
-                                  marginLeft: 10,
-                                  marginTop: 5
-                                },
+                                dateInput: ReserveStyles.input_style_date_input,
                                 dateText: ReserveStyles.input_style_date_time
                               }}
                               showIcon={false}
@@ -306,11 +342,11 @@ class Reserve extends Component {
                           <Icon
                             name="users"
                             type="font-awesome"
-                            color="rgba(110, 120, 170, 1)"
+                            color="#6e78aa"
                             size={25}
                           />
                         }
-                        placeholder="Diners"
+                        placeholder={i18n.t("reserve_form_diners")}
                         autoCapitalize="none"
                         autoCorrect={false}
                         keyboardType="number-pad"
@@ -325,14 +361,14 @@ class Reserve extends Component {
                         }
                         errorMessage={
                           this.state.comensales_error
-                            ? "Please, type how many diners"
+                            ? i18n.t("reserve_form_diners_error")
                             : null
                         }
                         containerStyle={ReserveStyles.input_container}
                         inputContainerStyle={
                           ReserveStyles.input_container_style
                         }
-                        placeholderTextColor="rgba(110, 120, 170, 1)"
+                        placeholderTextColor="#6e78aa"
                         inputStyle={ReserveStyles.input_style}
                         keyboardAppearance="light"
                         blurOnSubmit={false}
@@ -342,15 +378,11 @@ class Reserve extends Component {
                 </CardItem>
                 <CardItem
                   cardBody
-                  style={{
-                    marginTop: 30,
-                    alignContent: "center",
-                    justifyContent: "center"
-                  }}
+                  style={ReserveStyles.reserve_card_item_title_style}
                 >
-                  <H3>Datos del contacto</H3>
+                  <H3>{i18n.t("reserve_user_title").toUpperCase()}</H3>
                 </CardItem>
-                <CardItem cardBody style={{ marginTop: 20 }}>
+                <CardItem cardBody style={ReserveStyles.reserve_card_item_style}>
                   <Content>
                     <View style={ReserveStyles.view_form}>
                       <Input
@@ -358,11 +390,11 @@ class Reserve extends Component {
                           <Icon
                             name="user"
                             type="simple-line-icon"
-                            color="rgba(110, 120, 170, 1)"
+                            color="#6e78aa"
                             size={25}
                           />
                         }
-                        placeholder="First Name"
+                        placeholder={i18n.t("reserve_form_first_name")}
                         autoCapitalize="none"
                         autoCorrect={false}
                         keyboardType="email-address"
@@ -381,7 +413,7 @@ class Reserve extends Component {
                         inputContainerStyle={
                           ReserveStyles.input_container_style
                         }
-                        placeholderTextColor="rgba(110, 120, 170, 1)"
+                        placeholderTextColor="#6e78aa"
                         inputStyle={ReserveStyles.input_style}
                         keyboardAppearance="light"
                         blurOnSubmit={false}
@@ -391,11 +423,11 @@ class Reserve extends Component {
                           <Icon
                             name="screen-smartphone"
                             type="simple-line-icon"
-                            color="rgba(110, 120, 170, 1)"
+                            color="#6e78aa"
                             size={25}
                           />
                         }
-                        placeholder="Phone Number"
+                        placeholder={i18n.t("reserve_form_phone_number")}
                         autoCapitalize="none"
                         autoCorrect={false}
                         keyboardType="number-pad"
@@ -410,7 +442,7 @@ class Reserve extends Component {
                         inputContainerStyle={
                           ReserveStyles.input_container_style
                         }
-                        placeholderTextColor="rgba(110, 120, 170, 1)"
+                        placeholderTextColor="#6e78aa"
                         inputStyle={ReserveStyles.input_style}
                         keyboardAppearance="light"
                         blurOnSubmit={false}
@@ -420,11 +452,11 @@ class Reserve extends Component {
                           <Icon
                             name="email-outline"
                             type="material-community"
-                            color="rgba(110, 120, 170, 1)"
+                            color="#6e78aa"
                             size={25}
                           />
                         }
-                        placeholder="Email"
+                        placeholder={i18n.t("reserve_form_email")}
                         autoCapitalize="none"
                         autoCorrect={false}
                         keyboardType="email-address"
@@ -439,7 +471,7 @@ class Reserve extends Component {
                         inputContainerStyle={
                           ReserveStyles.input_container_style
                         }
-                        placeholderTextColor="rgba(110, 120, 170, 1)"
+                        placeholderTextColor="#6e78aa"
                         inputStyle={ReserveStyles.input_style}
                         keyboardAppearance="light"
                         blurOnSubmit={false}
@@ -462,10 +494,10 @@ class Reserve extends Component {
 
                             <Picker
                               mode="dropdown"
-                              iosIcon={<Icon name="arrow-down" />}
-                              style={{ marginLeft: 10 }}
-                              placeholder={"Credit Card"}
-                              placeholderStyle={{ color: "#6e78aa" }}
+                              // iosIcon={<Icon name="arrow-down" />}
+                              style={ReserveStyles.reserve_picker_style}
+                              placeholder={i18n.t("reserve_form_credit_card")}
+                              placeholderStyle={ReserveStyles.reserve_picker_color_style}
                               placeholderIconColor="#6e78aa"
                               selectedValue={this.state.id_credit_card}
                               onValueChange={value => this.setCreditCard(value)}
@@ -492,13 +524,13 @@ class Reserve extends Component {
                     </View>
                   </Content>
                 </CardItem>
-                <CardItem cardBody style={{ marginTop: 20 }}>
+                <CardItem cardBody style={ReserveStyles.reserve_card_item_style}>
                   <Content>
                     <View style={ReserveStyles.view_form}>
                       {this.props.reservaJSON == null ? (
                         <Button
                           loading={this.state.loading_reserve}
-                          title="REGISTER BOOKING"
+                          title={i18n.t("reserve_form_button_register").toUpperCase()}
                           containerStyle={
                             ReserveStyles.button_reserve_container_style
                           }
@@ -520,6 +552,10 @@ class Reserve extends Component {
   }
 }
 
+/**
+ * @description
+ * @param {*} state
+ */
 const mapStateToProps = state => {
   return {
     appJson: state.mainReducer.appJson,
@@ -529,10 +565,12 @@ const mapStateToProps = state => {
   };
 };
 
+/**
+ * @description
+ * @param {*} dispatch
+ */
 const mapDispatchToProps = dispatch => {
   return {
-    c_loadingTrue: () => dispatch(loadingTrue()),
-    c_loadingFalse: () => dispatch(loadingFalse()),
     c_deleteReserva: () => dispatch(deleteReserva()),
     c_addBookings: reservaJSON => dispatch(addBookings(reservaJSON))
   };
--- src/screens/Reserve/ReserveStyles.js
+++ src/screens/Reserve/ReserveStyles.js
@@ -1,23 +1,59 @@
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, Platform } from "react-native";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default StyleSheet.create({
+  //
   container: {
     flex: 1,
     backgroundColor: "#2E3248"
   },
+  //
+  reserve_scroll_view: {
+    flex: 1,
+    margin: 20
+  },
+  //
+  reserve_card_style: {
+    paddingBottom: 20
+  },
+  //
+  reserve_card_image: {
+    flex: 1,
+    height: 200,
+    width: null
+  },
+  //
+  reserve_card_item_title_style: {
+    marginTop: 30,
+    alignContent: "center",
+    justifyContent: "center"
+  },
+  //
+  reserve_card_item_style: {
+    marginTop: 20
+  },
+  //
+  reserve_date_picker_style: {
+    width: 200
+  },
+  //
+  reserve_picker_style: {
+    marginLeft: 10
+  },
+  //
+  reserve_picker_color_style: {
+    color: "#6e78aa"
+  },
+  //
   view_form: {
-    alignItems: "center",
-    // paddingBottom: 30
+    alignItems: "center"
   },
-  logo: {
-    marginTop: 20,
-    marginBottom: 20
-  },
+  //
   input_container: {
     width: SCREEN_WIDTH * 0.85
   },
+  //
   input_container_style: {
     borderRadius: 15,
     borderWidth: 1,
@@ -25,6 +61,7 @@ export default StyleSheet.create({
     height: 50,
     marginVertical: 10
   },
+  //
   input_style: {
     marginLeft: 10,
     color: "#2E3248"
@@ -41,9 +78,16 @@ export default StyleSheet.create({
     flexDirection: "row",
     borderRadius: 15,
     borderWidth: 1,
-    borderColor: "rgba(110, 120, 170, 1)",
+    borderColor: "#6E78AA",
     height: 50,
     marginVertical: 10
+  },
+  //
+  input_style_date_input: {
+    borderColor: "#FFF",
+    alignItems: "flex-start",
+    marginLeft: 10,
+    marginTop: 5
   },
   //
   input_style_date_time: {
@@ -57,25 +101,19 @@ export default StyleSheet.create({
     paddingLeft: 15,
     marginRight: -5
   },
+  //
   button_reserve_container_style: {
     marginTop: 10
   },
+  //
   button_reserve_style: {
     width: SCREEN_WIDTH * 0.8,
     borderRadius: 15,
     height: 50,
     backgroundColor: "#6E78AA"
   },
-  button_container_style: {
-    marginTop: 20
-  },
-  button_style: {
-    width: SCREEN_WIDTH * 0.8,
-    borderRadius: 15,
-    height: 50,
-    backgroundColor: "#3F51B5"
-  },
+  //
   button_title_style: {
-    fontFamily: "bold"
+    fontFamily: Platform.OS === "android" ? "bold" : null
   }
 });
